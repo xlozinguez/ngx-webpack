992e425e699efe449b6ed73825c967dc35111802
blacksonic <soos.gabor86@gmail.com>
2016-11-06 16:32:03 +0100

    refactor config generation

--- index.js
+++ index.js
@@ -1,27 +1,16 @@
 'use strict';
 
-let webpackGenerator = require('./config/webpack');
-let karmaGenerator = require('./config/karma');
+let webpackGenerator = require('./lib/webpack');
+let karmaGenerator = require('./lib/karma');
+let Config = require('./lib/config');
 
-let npmLifecycleEvent = process.env.npm_lifecycle_event || '';
-let environment = process.env.NODE_ENV || '';
-let singleRun = npmLifecycleEvent === 'test';
-let env = 'development';
-if (npmLifecycleEvent === 'build' || environment === 'production') {
-  env = 'production';
-} else if (npmLifecycleEvent.indexOf('test') !== -1 || environment === 'test') {
-  env = 'test';
-}
-
-let options = {
-  env, singleRun
-};
+let config = new Config();
 
 module.exports = {
-  webpack: function() {
-    return webpackGenerator(options);
+  webpack: function(options) {
+    return webpackGenerator(config.calculate(options));
   },
-  karma: function(webpackConfig) {
-    return karmaGenerator(webpackConfig, options);
+  karma: function(webpackConfig, options) {
+    return karmaGenerator(webpackConfig, config.calculate(options));
   }
 };

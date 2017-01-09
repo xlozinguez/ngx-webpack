0c4a7bd7e60f7b9d431e81207643fa3d9253f5f4
blacksonic <soos.gabor86@gmail.com>
2017-01-09 11:04:27 +0100

    remove source map loader and add aot lib to example

--- src/app/modules/main.module.ts
+++ src/app/modules/main.module.ts
@@ -1,11 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
+import { TranslateModule } from 'ng2-translate';
 import { HelloWorldComponent } from '../components/hello-world/hello-world.component';
 
 @NgModule({
   bootstrap: [HelloWorldComponent],
   declarations: [HelloWorldComponent],
-  imports: [BrowserModule],
+  imports: [
+    BrowserModule,
+    HttpModule,
+    TranslateModule.forRoot()
+  ],
   providers: []
 })
 export class MainModule {}
